4222021998b96103615ab3e2340447c2b6c3246f
Jerome Flaherty <jayromulus.f@gmail.com>
2020-02-11 18:36:52 -0500

    animate this later

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 // document.getElementsByClassName('nav-item')
 
-$(window).scroll(function() {
+// $(window).scroll(function() {
 
-    //After scrolling 100px from the top...
-    if ( $(window).scrollTop() >= 100 ) {
-        $('#logo').css('display', 'none');
-        $('#menuheader').css('margin', '65px auto 0');
+//     //After scrolling 100px from the top...
+//     if ( $(window).scrollTop() >= 100 ) {
+//         $('#logo').css('display', 'none');
+//         $('#menuheader').css('margin', '65px auto 0');
 
-    //Otherwise remove inline styles and thereby revert to original stying
-    } else {
-        $('#logo, #menuheader').attr('style', '');
+//     //Otherwise remove inline styles and thereby revert to original stying
+//     } else {
+//         $('#logo, #menuheader').attr('style', '');
 
-    }
+//     }
+// });
+
+
+$(window).scroll(function(){            
+    $("div").css("margin-top", $(window).scrollTop())   
 });
+
+console.log($(window).width);
